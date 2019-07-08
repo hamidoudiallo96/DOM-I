3ce6636a70429392d7dd9519a326e45756ba67d3
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-07-08 18:41:51 -0400

    timer added

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,15 +5,15 @@ const msTens = document.querySelector("#msTens");
 
 
 
-let ms1 = 0;
 let ms10 = 0;
+let ms100 = 0;
 let sec1 = 0;
 let sec10 = 0;
 
-secondTens.textContent = sec10;
-secondOnes.textContent = sec1;
-msHundreds.textContent = ms10;
-msTens.textContent= ms1;
+// secondTens.textContent = sec10;
+// secondOnes.textContent = sec1;
+// msHundreds.textContent = ms100;
+// msTens.textContent= ms1;
 
 
 // creating button tag and adding it to body
@@ -53,41 +53,62 @@ buttons.forEach(item => {
 
 button.addEventListener("click", item => {
     console.log("Hey");
-    ms10+=1;
-    setTimeout(function (){
+    
+    
+    setInterval(function (){
+        // console.log("Sean");
+        ms10+=1;
         MilliSecond();
         firstSecond();
         tenSecond();
+        stopper();
 
     },1000);
 
 
     
-});
+},true);
 
 function MilliSecond(){
-    if(ms10 === 9){
-       ms10 = 0;
-    }else{
-        ms1 +=1 ;
+    if(ms10 === 6){
+        ms10 = 0;
+        ms100 +=1 ;
     }
+    console.log(ms10);
+    msTens.textContent= ms10;
 }
 
 function firstSecond(){
-    if(ms1 === 9){
-        ms1 = 0;
-    }else{
-        sec10 +=1;
+    if(ms100 === 6){
+        ms100 = 0;
+        sec1 +=1;
     }
+    console.log(ms100);
+    msHundreds.textContent= ms100;
 }
 
 function tenSecond(){
-    if(sec10 === 9){
-        sec10 = 0;
-    }else{
-        sec1 +=1;
+    if(sec1 === 6){
+        sec1 = 0;
+        sec10 +=1;
     }
+    console.log(sec1);
+    secondOnes.textContent = sec1;
 }
+
+function stopper(){
+    if(sec10 === 6){
+        sec10 = 0;
+        clearTimeout();
+    }
+    console.log(sec10);
+    secondTens.textContent= sec10;
+}
+
+
+closeButton.addEventListener("click", ev =>{
+    
+});
 
 
 
